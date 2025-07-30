2b5131c7e6f99b4b81e06c7762bc278afc545b4d
Rishita Rawat <rishitarawat1000@gmail.com>
2025-07-30 17:31:29 +0530

    Initial working version of task manager API

---
+++ src/main/java/com/example/taskapi/controller/TaskController.java
@@ -0,0 +1,48 @@
+package com.example.taskapi.controller;
+
+import com.example.taskapi.entity.Task;
+import com.example.taskapi.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setDueDate(taskDetails.getDueDate());
+        return taskRepository.save(task);
+    }
+
+    @PatchMapping("/{id}/complete")
+    public Task markComplete(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setCompleted(true);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
+
